b529a6a762504b22e38d64e08f1a2c6c18d6a209
MaxCheng <nokia6102@gmail.com>
2017-08-07 18:46:51 +0800

    做到第一頁UI畫面

---
+++ CloudsAPP/StudentDataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentDataTableViewCell.swift
+//  CloudsAPP
+//
+//  Created by MaxCheng on 2017/8/7.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import UIKit
+
+class StudentDataTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
